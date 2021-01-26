b45bc99f7bc1a9dad566feead2d85823c68e014a
Your Name <you@example.com>
2021-01-26 05:07:27 -0800

    added

---
+++ java.js
@@ -0,0 +1,152 @@
+/*let iteration = 1;
+while (iteration <= 5){
+    document.write(iteration);
+    iteration++;
+}
+*/
+
+
+
+let i = 35;
+while (i >= 11){
+    document.write ( i + "<br>" );//3linya skobka koebuz
+    i--; 
+}
+
+
+
+/*
+//1 primerge okshosh rare use 
+for (let i3 =5; i3 >= 1; i3++){
+    document.write (i3 + "<br>")
+}
+*/
+
+/*
+let i2 = 5;
+do {
+    document.write(i2 + "<br>");
+    i2--;
+} while (12 >= 1 )
+*/
+
+//ne chetnye chisly && chetnye chisly
+/*
+let i = 25;
+while (i >= 1){
+    if (i % 3 == 0)
+    document.write ( i + "<br>" );//3linya skobka koebuz
+}
+    i--; 
+
+
+let i = 26;
+while (i >= 1){
+    if (i % 2 == 1)
+    document.write ( i + "<br>" );//3linya skobka koebuz
+}
+    i--; 
+
+*/
+/*
+let i = 25;
+while (i >= 1){
+    if (i % 2 != 0)
+    document.write ( i + "<br>" );//3linya skobka koebuz
+}
+    i--; 
+    */
+
+    /*//pochti 
+let i = 25;
+while (i >= 1){
+    if (i % 3 == 0)
+    document.write ( i + "<br>" );
+    i--; 
+}
+let i2 = 25;
+do {
+    document.write(i2 + "<br>");
+    i2--;
+} while (12 >= 1 )
+*/
+
+/*
+let i = 15;
+while (i >= 1){
+    document.write ( i + "<br>" );
+     
+    if (i == 10) {
+        break;
+    }
+
+    i--; 
+}
+*/
+  
+///let i = +prompt("vvedite chislo");
+//while (i >= 1){
+  //  document.write ( i + "<br>" );
+     
+   /* if (i == 10) {
+        break;
+    }*/
+
+  //  i--; 
+//}
+
+//3 ton 100go
+/*
+let i = 100;
+let stop = +prompt("vvedite chislo");
+while (i >= 1){
+    document.write ( i + "<br>" );
+     
+   if (i == stop) {
+        break;//while
+    }
+
+    i--; 
+}*/
+/*
+let i = 1;
+while (i <= 20){
+    if (i % 2 == 1){
+    document.write(i);
+    }
+    i++;
+}
+
+let x = 1;
+while (x <= 20){
+    x++;
+    if (x % 2 == 1){//(x % 2 !=0)
+    continue;
+    }
+    document.write(x + "<br>");
+    
+}*/
+
+
+//while (true) //sozdaet beskonechnye sikly
+/*
+let answer = prompt("please input number below 100")
+let number = parseInt(answer)//use how if 
+document.write((10 > number))
+*/
+/*
+let answer, number;
+//let answer = 0,
+//number = 1
+//let answer
+//let number mozhem zadat peremennye no ne zadavat
+while (true){
+answer = prompt("please input number below 100")
+number = parseInt(answer);
+    if (100 > number){
+        document.write("number is" +number)
+        break;
+    }
+}*/
+let num = 100
+while 
